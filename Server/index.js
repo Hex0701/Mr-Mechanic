68bf73255456199d5398bbe065c001e05c06fa5c
aakash aman <aakash.aman@student.sl.on.ca>
2025-01-25 15:28:11 -0500

    initial commit

---
+++ Server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 8080;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello from Express!');
+});
+
+app.post('/data', (req, res) => {
+    const { name } = req.body;
+    res.send({ message: `Hello, ${name}!` });
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
